3f79dd198b106fdee93f225317e28d1206c4acc0
KurtSpire <dddovzhik@gmail.com>
2025-09-17 16:22:45 +0200

    Add files via upload

---
+++ dovz_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("Mój numer w dzienniku to 7"+"<br>");
+document.write("Pierwszy wierszto :&nbsp;&nbsp;11&nbsp;&nbsp;Tak&nbsp;&nbsp;Tak&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5"+"<br>");
+document.write("Drugi wiersz to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10&nbsp;&nbsp;Tak&nbsp;&nbsp;Nie&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;6"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#808000").bold().fontsize(5).strike()+"<br>"); 
+document.write(document.lastModified.fontcolor("#00FF00").bold());
